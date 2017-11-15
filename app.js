e4de3f77c72ca418fdc330fa54a2e1a588e370ab
hebergb <hebergb98@gmail.com>
2017-11-14 22:49:11 -0600

    Apenas la primer fila :'v

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 var esquema = "";
 
 function calcular() {
-    var monto = document.getElementById("monto").nodeValue
-    var periodo = document.getElementById("periodo").nodeValue
-    var interes = document.getElementById("tasa-interes").nodeValue
-    switch (esquema) {
-        case "decreciente":
-            function decreciente() {
+  var monto = document.getElementById("monto").nodeValue
+  var periodo = document.getElementById("periodo").nodeValue
+  var interes = document.getElementById("tasa-interes").nodeValue
+  switch (esquema) {
+    case "decreciente":
+      function decreciente() {
+        var fila = 
+        `<tr>
+          <td>$${i}</td>
+          <td>$${inicial}</td>
+          <td>$${amortizacion}</td>
+          <td>$${interes}</td>
+          <td>$${pago}</td>
+          <td>$${final}</td>
+        </tr>`
+      }
+      break;
+    case "creciente":
+      function creciente() {
 
-            }
-            break;
-        case "creciente":
-            function creciente() {
+      }
+      break;
+    case "bullet":
+      function bullet() {
 
-            }
-            break;
-        case "bullet":
-            function bullet() {
+      }
+      break;
+    case "igual":
+      function igual() {
 
-            }
-            break;
-        case "igual":
-            function igual() {
+      }
+      break;
 
-            }
-            break;
+    default:
+      break;
+  }
+}
 
-        default:
-            break;
-    }
+function cambiarMain(info){
+  if(info=='fundamentacion'){
+    document.getElementById('portada').style.display="none";
+    document.getElementById("fundamentacion").style.display="block";
+  } else {
+    document.getElementById('portada').style.display="flex";
+    document.getElementById('fundamentacion').style.display="none";
+  }
 }
