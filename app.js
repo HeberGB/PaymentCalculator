910599dbb6988dc60b490092bb97ad6d6cb71af5
hebergb <hebergb98@gmail.com>
2017-11-14 21:21:30 -0600

    Medios Estilos

---
+++ app.js
@@ -0,0 +1,32 @@
+var esquema = "";
+
+function calcular() {
+    var monto = document.getElementById("monto").nodeValue
+    var periodo = document.getElementById("periodo").nodeValue
+    var interes = document.getElementById("tasa-interes").nodeValue
+    switch (esquema) {
+        case "decreciente":
+            function decreciente() {
+
+            }
+            break;
+        case "creciente":
+            function creciente() {
+
+            }
+            break;
+        case "bullet":
+            function bullet() {
+
+            }
+            break;
+        case "igual":
+            function igual() {
+
+            }
+            break;
+
+        default:
+            break;
+    }
+}
